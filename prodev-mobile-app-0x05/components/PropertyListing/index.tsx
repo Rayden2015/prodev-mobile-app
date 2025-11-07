fc069303bf1d5d74e6d56d4154ecfa967e53c846
Nurudin Wahab Lartey <nurundin2010@gmail.com>
2025-11-07 11:45:02 +0000

    updates

--- prodev-mobile-app-0x05/components/PropertyListing/index.tsx
+++ prodev-mobile-app-0x05/components/PropertyListing/index.tsx
@@ -1,6 +1,6 @@
 import { View } from "react-native";
 
-import PropertyListingCard from "./common/PropertyListingCard";
+import PropertyListingCard from "../common/PropertyListingCard";
 import { PropertyListingProps } from "@/interfaces";
 
 type PropertyListingListProps = {
